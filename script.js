a45f31e0fe8400a1bab0bfb9dafdf9144998b9bd
MohamedAli1186 <Mohammedaallii10@gmail.com>
2024-09-07 21:27:11 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let products = [];
+let cards = document.querySelector(".cards");
+
+let getProducts = new Promise((suc, fail) => {
+    fetch("https://dummyjson.com/products?limit=10")
+    .then((response) => response.json())
+    .then((data) => {
+    suc(data["products"]);
+    })
+    .catch((er) => fail("No Data fetched"));
+});
+
+getProducts.then((data) => {
+    products = data;
+    for (let product of products) {
+    cards.innerHTML += `
+    <div class="card" onclick="redirectToDetails(${product.id})">
+        <img src="${product.thumbnail}" alt="product" />
+        <h3>${product.title}</h3>
+        <p>${product.price}</p>
+    </div>`;
+    } 
+});
+
